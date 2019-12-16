1e19870b7362c9c9d6b26794ba0d46e80c2319be
jneo8 <jneo8lin@gmail.com>
2019-12-16 18:41:04 +0800

    parse osm pbf

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Main."""
+from pkg.osm_parser.pbf_parser import pbf_parser
+
+
+def main():
+    """Main."""
+    pbf_parser("/home/jameslin/ghtinc-Porject/Geo-Analyst/src/taiwan-latest.osm.pbf")
+
+
+if __name__ == "__main__":
+    main()
